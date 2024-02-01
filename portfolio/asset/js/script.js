5cadc1a40512d1f1dfe328b6471ecfcf7717e82b
MDI <122687470+gitmehdii@users.noreply.github.com>
2024-02-01 00:14:26 +0000

    update portfolio/index.html

--- portfolio/asset/js/script.js
+++ portfolio/asset/js/script.js
@@ -1,15 +1,18 @@
 // Ajoutez ces scripts pour changer la couleur du fond en fonction du défilement
 document.addEventListener("DOMContentLoaded", function () {
-    const backgroundAnimation = document.querySelector('.background-animation');
-  
-    window.addEventListener('scroll', function () {
-      const scrollPosition = window.scrollY;
-  
-      // Changez les couleurs en fonction de la position du défilement
-      if (scrollPosition % 2 === 0) {
-        backgroundAnimation.style.background = 'linear-gradient(45deg, #121212, #050a2e)';
-      } else {
-        backgroundAnimation.style.background = 'linear-gradient(45deg, #050a2e, #121212)';
-      }
-    });
+  const backgroundAnimation = document.querySelector('.background-animation');
+
+  window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+
+    // Changez les couleurs en fonction de la position du défilement avec transition
+    if (scrollPosition % 2 === 0) {
+      backgroundAnimation.style.background = 'linear-gradient(45deg, #121212, #050a2e)';
+    } else {
+      backgroundAnimation.style.background = 'linear-gradient(45deg, #050a2e, #121212)';
+    }
+
+    // Ajoutez une transition pour lisser le changement de couleur
+    backgroundAnimation.style.transition = 'background 0.5s ease';
   });
+});
