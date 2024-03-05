b5e72b7fc65481817d72e40a07341ab7914810a4
MDI <mehdi.azouz@epita.fr>
2024-03-05 14:07:28 +0000

    react

---
+++ react/app-react-basic/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './Petite.css'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ react/app-react-basic/src/MainContent.jsx
@@ -0,0 +1,13 @@
+import corporateImg from './assets/img/corporate.jpg';
+function MainContent() {
+    return (
+        <main>
+            <section>
+                <img src={corporateImg} alt="Corporate" />
+                <p>We leverage agile frameworks to provide a robust synopsis for high-level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+            </section>
+        </main>
+    )
+}
+
+export default MainContent;
