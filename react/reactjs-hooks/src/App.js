1254973c9157d0fce2f774799dc4b45e5394af08
MDI <mehdi.azouz@epita.fr>
2024-03-05 14:41:47 +0000

    react 2

---
+++ react/reactjs-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IncrementDecrement from './IncrementDecrement';
+import './Counter.css';
+
+function App () {
+    return (
+        <div className="box-counter">
+            <IncrementDecrement></IncrementDecrement>
+        </div>
+    )
+}
+
+export default App;
